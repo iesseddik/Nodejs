b9a689bc67a0e8d3923440385fdfcf1b26b6507f
Esseddik ismael <ismael.esseddik@gmail.com>
2018-02-03 17:41:13 +0100

    dispatchingScripts

---
+++ Hotelmultiplication.js
@@ -0,0 +1,29 @@
+var http = require('http'); // appel a la bibliotheque de nodejs
+var url = require("url");// import du module url
+var querystring = require('querystring');
+var express = require('express');
+var app = express(); // npm express en ligne de commande avant
+
+
+app.get('/', function(req, res) { // creation de route(url), ici root
+  res.setHeader('Content-Type','Text/plain');
+  res.end('vous etes a l\'accueil');
+});
+
+/*
+app.get('/:etagenum/:chambrenum', function(req, res) {
+  res.setHeader('Content-Type','Text/plain');
+  console.log(req.params);
+  res.end('vous etes a l\'etage '+ req.params.etagenum +' chambre '+ req.params.chambrenum);
+});ou alors...*/
+
+app.get('/:etagenum/:chambrenum', function(req, res) { //neccesite npm install ejs, creation dossier views, script.ejs
+    res.render('etagechambre.ejs', {etage: req.params.etagenum, chambre: req.params.chambrenum, compteur: req.params.etagenum*req.params.chambrenum });
+});
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(8081);
--- welcom.js
+++ welcom.js
@@ -2,6 +2,7 @@ var http = require('http'); // appel a la bibliotheque de nodejs
 var url = require("url");// import du module url
 var querystring = require('querystring');
 
+
 var server = http.createServer(function(req, res) {
     var page = url.parse(req.url).pathname;
     var query = url.parse(req.url).query
@@ -25,34 +26,6 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(8080); // ecoute sur le port 8080
-
 server.on('close', function() { // On écoute l'évènement close
     console.log('Bye bye !');
 })
-
-
-var express = require('express');
-var app = express(); // npm express en ligne de commande avant
-
-
-app.get('/', function(req, res) { // creation de route(url), ici root
-  res.setHeader('Content-Type','Text/plain');
-  res.end('vous etes a l\'accueil');
-});
-
-/*
-app.get('/:etagenum/:chambrenum', function(req, res) {
-  res.setHeader('Content-Type','Text/plain');
-  console.log(req.params);
-  res.end('vous etes a l\'etage '+ req.params.etagenum +' chambre '+ req.params.chambrenum);
-});ou alors...*/
-app.get('/:etagenum/:chambrenum', function(req, res) { //neccesite npm install ejs, creation dossier views, script.ejs
-    res.render('etagechambre.ejs', {etage: req.params.etagenum, chambre: req.params.chambrenum, compteur: req.params.etagenum*req.params.chambrenum });
-});
-
-app.use(function(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('Page introuvable !');
-});
-
-app.listen(8081);
