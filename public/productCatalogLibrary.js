f4c7edb583fb69c8aa9ad198196ceb62fa70b8b0
Esseddik ismael <ismael.esseddik@gmail.com>
2018-03-04 17:06:41 +0100

    edx promiselib complete

--- public/productCatalogLibrary.js
+++ public/productCatalogLibrary.js
@@ -1,35 +1,117 @@
-var count = 0;
-var activation = false;
-var records = document.getElementById("records");
+(function(window){
 
-document.getElementById('startstop').onclick = function(){
-  if (activation === false){   //si inactif au moment du click on l'active
-    console.log(activation);
-    activation = true
-  }else { //sinon on le desactive
-    console.log(activation)
-    activation = false
-  }
-}
+    function myLibrary(){
 
-setInterval(function () { //on va toute les seconde afficher puis si actif incrementer
-  document.getElementById('timer').innerHTML = count;
-  if (activation=== true){
-    count+=0.01;
-  }
-  console.log(count);
-},100)
+        //execute code here
+        var catalog = createRandomCatalog(100);
 
 
+        return {
+            searchProductById: searchProductById,
+            searchProductsByPrice: searchProductsByPrice,
+            searchProductsByType: searchProductsByType,
+            searchAllProducts: searchAllProducts
+        }
 
-document.getElementById('reset').onclick = function(){
-  count = 0;
-  while (records.firstChild) {
-    records.removeChild(records.firstChild);
-  }
-}
+        //function definitions go here
+        function createRandomProduct(){
+          var typeArray = ['Electronics','Book','Clothing','Food'];
+          var price = (Math.random()*500).toFixed(2)
+          var type = typeArray[Math.floor(Math.random()*4)];
 
-document.getElementById('recordtime').onclick = function(){
-  var tr = document.createElement("tr");
-  records.appendChild(tr).innerHTML = count;
-}
+          return {price:price, type:type};
+        }
+
+        function createRandomCatalog(num){
+          var catalog = [];
+          for (var i = 0; i < num; i++){
+              var obj = createRandomProduct();
+              catalog.push({id:i,price:obj.price,type:obj.type});
+          }
+          return catalog;
+        }
+
+        function searchAllProducts(){
+          var promise = new Promise(function(resolve, reject) {
+
+              setTimeout(function(){
+                  resolve(catalog);
+              },1000);
+
+          });
+          return promise;
+        }
+
+
+        function searchProductById(id){
+          var promise = new Promise(function(resolve,reject){
+              var i = 0;
+              setTimeout(function(){
+                  while (i < catalog.length){
+                      if (catalog[i].id == id){
+                          resolve({id:id,price:catalog[i].price,type:catalog[i].type});
+                      }
+                      i++;
+                  }
+                  reject("Invalid ID: " + id);
+              },1000);
+          });
+          return promise;
+        }
+
+        function searchProductsByType(type){
+            var promise = new Promise(function(resolve,reject){
+                var i = 0;
+                var typeArray = [];
+                var possibleTypes = ['Electronics','Book','Clothing','Food'];
+                if(!possibleTypes.includes(type)){
+                    reject("Invalid Type: " + type)
+                }
+                else{
+                    setTimeout(function(){
+                        while (i < catalog.length){
+                            if (catalog[i].type == type){
+                                typeArray.push({id:catalog[i].id,price:catalog[i].price,type:catalog[i].type});
+                            }
+                            i++;
+                        }
+                        resolve(typeArray);
+                    },1000);
+                }
+            });
+            return promise;
+        }
+
+        function searchProductsByPrice(price,difference){
+            var promise = new Promise(function(resolve,reject){
+                var i = 0;
+                var priceArray = [];
+                if(!isFinite(price)){
+                    reject("Invalid Price: " + price)
+                }
+                else{
+                    setTimeout(function(){
+                        while (i < catalog.length){
+                            if (Math.abs(catalog[i].price - price) < difference){
+                                priceArray.push({id:catalog[i].id,price:catalog[i].price,type:catalog[i].type});
+                            }
+                            i++;
+                        }
+                        resolve(priceArray);
+                    },1000);
+                }
+            });
+            return promise;
+        }
+
+
+
+
+    }
+
+
+    if(typeof(window.api) === 'undefined'){
+        window.api = myLibrary();
+    }
+
+})(window);
